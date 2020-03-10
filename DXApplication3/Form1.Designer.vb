85c7b86378708a9471813e6cd02e11a676edb581
arkhanan <arkhanan@gmail.com>
2020-03-10 11:47:00 -0600

    boton

--- DXApplication3/Form1.Designer.vb
+++ DXApplication3/Form1.Designer.vb
@@ -24,18 +24,32 @@
     ''' the contents of this method with the code editor.
     ''' </summary>
     Private Sub InitializeComponent()
+        Me.ButtonEdit1 = New DevExpress.XtraEditors.ButtonEdit()
+        CType(Me.ButtonEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'ButtonEdit1
+        '
+        Me.ButtonEdit1.Location = New System.Drawing.Point(39, 35)
+        Me.ButtonEdit1.Name = "ButtonEdit1"
+        Me.ButtonEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
+        Me.ButtonEdit1.Size = New System.Drawing.Size(100, 20)
+        Me.ButtonEdit1.TabIndex = 0
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(870, 540)
+        Me.Controls.Add(Me.ButtonEdit1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.ButtonEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents ButtonEdit1 As DevExpress.XtraEditors.ButtonEdit
 
 #End Region
 
